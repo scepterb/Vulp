fe044a3d986c3de6a41ad62189a5e9d9a4635af2
Ben <bcberedo17@gmail.com>
2026-07-08 20:10:50 -0400

    Initial commit I guess I should start a GitHub repo for this huh

---
+++ lib/IO/src/inc/io.h
@@ -0,0 +1,36 @@
+/**
+ * @file io.cpp
+ * @author Ben Beredo
+ * @date 2026
+ */
+
+#ifndef __IO__
+#define __IO__
+
+#include <Arduino.h>
+#include <stdio.h>
+
+#define MSG_MAGIC '%'
+#define MSG_STOP '!'
+
+void setupSerial();
+int read_byte_blocking();
+void read_header();
+
+/** @brief Reads bytes from serial, blocking, until the stop byte is reached.
+ * 
+ *  @param buf A pointer to a buffer to store the bytes.
+ *  @param len The length of the buffer (to avoid overflow)
+ */
+void read_bytes(void *buf, uint16_t len);
+
+/** @brief Reads a message from serial.
+ * 
+ *  @param buf A pointer to a buffer to store the incoming message.
+ *  @param len The length of the buffer (to avoid overflow)
+ * 
+ *  @return 0 on success. -1 if overflow detected.
+ */
+void read_message(void *buf, uint16_t len);
+
+#endif
---
+++ lib/IO/src/io.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file io.cpp
+ * @author Ben Beredo
+ * @date 2026
+ */
+
+#include "inc/io.h"
+
+int read_byte_blocking() {
+  while (!Serial.available()) {}
+  int incomingByte = Serial.read();  // will not be -1
+  return incomingByte;
+}
+
+/** @brief Reads bytes from serial, blocking, until the stop byte is reached.
+ * 
+ *  @param buf A pointer to a buffer to store the bytes.
+ *  @param len The length of the buffer (to avoid overflow)
+ */
+void read_bytes(void *buf, uint16_t len) {
+  int incomingByte = 0;
+
+  for (int i = 0; i < len; i++) {
+    incomingByte = read_byte_blocking();
+
+    if (incomingByte == MSG_STOP) {
+      break;
+    }
+    ((uint8_t *)buf)[i] = incomingByte;
+  }
+}
+
+void read_header() {
+  int magic = read_byte_blocking();
+  while (magic != MSG_MAGIC) {
+    magic = read_byte_blocking();
+  }
+}
+
+/** @brief Reads a message from serial.
+ * 
+ *  @param buf A pointer to a buffer to store the incoming message.
+ *  @param len The length of the buffer (to avoid overflow)
+ */
+void read_message(void *buf, uint16_t len)
+{
+  read_header();
+
+  if (buf != NULL) {
+    read_bytes(buf, len);
+  }
+}
+
+void setupSerial() {
+  Serial.begin(115200);
+  while (!Serial) {}
+}
